7024ab6a290e47dbdcc3dc48cda3d0f4556f555c
JOON HYUK LEE <60730765+anshqhsh@users.noreply.github.com>
2021-09-06 18:17:20 +0900

    res & req

--- 5-express/app.js
+++ 5-express/app.js
@@ -5,14 +5,19 @@ const app = express();
 app.get('/sky/:id', (req, res, next) => {
   // console.log('path : ', req.path);
   // console.log('headers : ', req.headers);
-  console.log('params : ', req.params);
+  console.log(req.params);
   console.log(req.params.id);
   //http://localhost:8080/sky/joonhyuk/?keyword=bts = query :  { keyword: 'bts' }
   console.log('query : ', req.query);
   console.log(req.query.keyword);
-  res.send('hi');
+
+  // res.json({ name: 'joonhyuk' });
+  // res.json({name : joon});
+  // res.sendStatus(400);
+  res.status(201).send('created');
+  res.setHeader('key', 'value'); // header에 필여한 것을 세팅
 }); // 메소드를 이용
-app.listen(8080);
+app.listen(8081);
 
 //IP
 //PORT 서버의 IP 주소 어떤 어플리케이션에 접속할지 알려 주는 것
